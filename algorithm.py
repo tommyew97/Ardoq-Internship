e21ade12cf5ba5642453ca1219d63cf31a3f371d
Tommy Woldseth <tommyew7@gmail.com>
2021-09-12 15:06:58 +0200

    Available bikes functionality and remove some algorithms

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,11 @@
-def product_of_three_largest_numbers(numbers):
-    numbers.sort()
-    three_largest = numbers[-3:]
-    sum = three_largest[0]
-    for i in range(1, len(three_largest)):
-        sum *= three_largest[i]
-    return sum
+# Algorithms for finding the product of the three largest numbers in a list.
+# Two solutions, one using the .sort()-function and one which doesn't use this function.
 
-def product_of_three_largest_numbers2(numbers):
+def product_of_three_largest_numbers(numbers):
     numbers.sort()
     return numbers[-3]*numbers[-2]*numbers[-1]
 
-def product_of_three_larges_numbers3(numbers):
+def product_of_three_largest_numbers2(numbers):
     largest = find_largest_in_list(numbers)
     numbers.remove(largest)
     next_largest = find_largest_in_list(numbers)
@@ -24,12 +19,3 @@ def find_largest_in_list(numbers):
         if el > largest:
             largest = el
     return largest
-
-def product_of_three_largest_numbers4(numbers):
-    result = max(numbers)
-    numbers.remove(result)
-    for i in range(2):
-        largest = max(numbers)
-        result *= largest
-        numbers.remove(largest)
-    return result
